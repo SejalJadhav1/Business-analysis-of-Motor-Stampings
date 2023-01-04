48ed5295821f6de83a9367e19de43dfe01455814
Sejal Jadhav <89533449+SejalJadhav1@users.noreply.github.com>
2023-01-04 13:54:26 +0530

    Update 2_Products.py

--- pages/2_Products.py
+++ pages/2_Products.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
 import plotly.express as px
+import openpyxl
 
 st.set_page_config(
     page_title="Products"
